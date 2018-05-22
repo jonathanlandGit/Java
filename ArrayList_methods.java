146b2db415bcaf56d2a2c36746c2c1ab7cc480c4
Jonathan Land <jonathan.land77@gmail.com>
2018-05-21 22:47:53 -0400

    Update ArrayList_methods.java

--- ArrayList_methods.java
+++ ArrayList_methods.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 /*
  * @author Jonathan Land
  * 
- * Program that includes various methods using arraylist data structure
+ * Program that executes various methods using an arraylist data structure with randomized values each time the program runs.
  */
 
 public class ArrayList_methods 
